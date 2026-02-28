1411fa8af761b0366e489ac7bec52ce367f2fb6a
Jose Rodriguez <pasanterecursosinformaticos@servinsaludipssas.com>
2026-02-28 11:29:37 -0500

    Se mejora dashboards y visuales

--- tickets/forms.py
+++ tickets/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Ticket
+from django.contrib.auth.forms import AuthenticationForm
 
 class TicketForm(forms.ModelForm):
     class Meta:
@@ -9,3 +10,16 @@ class TicketForm(forms.ModelForm):
             "descripcion",
             "prioridad"
         ]
+        
+class LoginForm(AuthenticationForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control'
+        })
+
+        self.fields['password'].widget.attrs.update({
+            'class': 'form-control'
+        })
